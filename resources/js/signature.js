47110f6f5c31818c4f096670f07d6c855daa7f23
Muhammad Hanis Irfan <hanisirfan.work@gmail.com>
2021-08-09 21:11:13 +0800

    Added a drawing pad to be used to generate signature PNGs with ratio needed for certificats (3:1 ratio).

---
+++ resources/js/signature.js
@@ -0,0 +1,91 @@
+import SignaturePad from '../../node_modules/signature_pad/dist/signature_pad';
+
+var canvasWrapper = document.getElementById('signature-pad-wrapper');
+var canvas = canvasWrapper.querySelector('#signature-pad');
+var saveBtn = document.querySelector('#signature-pad-save');
+var undoBtn = document.querySelector('#signature-pad-undo');
+var clearBtn = document.querySelector('#signature-pad-clear');
+
+var signaturePad = new SignaturePad(canvas, {
+    backgroundColor: 'rgba(255, 255, 255, 0)',
+    penColor: 'rgb(0, 0, 0)',
+    minWidth: 3,
+    maxWidth: 5,
+});
+
+/**
+ * Some of the code are taken from the Signature Pad demo
+ * https://github.com/szimek/signature_pad
+ */
+ function download(dataURL, filename) {
+    var blob = dataURLToBlob(dataURL);
+    var url = window.URL.createObjectURL(blob);
+  
+    var a = document.createElement("a");
+    a.style = "display: none";
+    a.href = url;
+    a.download = filename;
+  
+    document.body.appendChild(a);
+    a.click();
+  
+    window.URL.revokeObjectURL(url);
+}
+
+function dataURLToBlob(dataURL) {
+    // Code taken from https://github.com/ebidel/filer.js
+    var parts = dataURL.split(';base64,');
+    var contentType = parts[0].split(":")[1];
+    var raw = window.atob(parts[1]);
+    var rawLength = raw.length;
+    var uInt8Array = new Uint8Array(rawLength);
+  
+    for (var i = 0; i < rawLength; ++i) {
+      uInt8Array[i] = raw.charCodeAt(i);
+    }
+  
+    return new Blob([uInt8Array], { type: contentType });
+}
+
+// Save PNG image
+var today = new Date();
+var date = today.getFullYear()+'_'+(today.getMonth()+1)+'_'+today.getDate();
+
+saveBtn.addEventListener('click', function (event) {
+    if (signaturePad.isEmpty()) {
+        alert("Sila tuliskan tandatangan anda dahulu.");
+    } else {
+        var dataURL = signaturePad.toDataURL();
+        download(dataURL, "SeaJell_Tandatangan_" + date + ".png");
+    }
+});
+
+
+// Undo move
+undoBtn.addEventListener("click", function (event) {
+    var data = signaturePad.toData();
+  
+    if (data) {
+      data.pop(); 
+      signaturePad.fromData(data);
+    }
+});
+  
+// CLear pad
+clearBtn.addEventListener('click', function (event) {
+    signaturePad.clear();
+});
+
+function resizeCanvas() {
+    var ratio =  Math.max(window.devicePixelRatio || 1, 1);
+    canvas.width = canvas.offsetWidth * ratio;
+    canvas.height = canvas.offsetHeight * ratio;
+    console.log(canvas.width);
+    console.log(canvas.height);
+    canvas.style.border = 'solid #000' 
+    canvas.getContext("2d").scale(ratio, ratio);
+    signaturePad.clear(); // otherwise isEmpty() might return incorrect value
+}
+
+window.addEventListener("resize", resizeCanvas);
+resizeCanvas();
